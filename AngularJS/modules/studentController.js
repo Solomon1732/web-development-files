3f7069f019e5016f52c53e7997644e2395e673dd
Solomon1732 <shlomireu5@gmail.com>
2016-01-28 20:07:16 +0200

    Added files for angular practice

---
+++ AngularJS/modules/studentController.js
@@ -0,0 +1,20 @@
+mainApp.controller("studentController", ["$scope", function($scope) {
+	$scope.student = {
+		firstName: "John",
+		lastName: "Doe",
+		fees: 500,
+
+		subjects: [
+			{name: 'Physics', marks: 70},
+			{name: 'Chemistry', marks: 80},
+			{name: 'Math', marks: 65},
+			{name: 'English', marks: 75},
+			{name: 'Arabic', marks: 67},
+		],
+
+		fullName: function() {
+			var studentObject = $scope.student;
+			return studentObject.firstName + " " + studentObject.lastName;
+		}
+	};
+}]);
