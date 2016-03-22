bd5b63e1cab22d55ff0e8cfc7758bf4b5417a00d
Solomon1732 <shlomireu5@gmail.com>
2016-03-22 11:30:06 +0200

    Worked on files in directory: AngularJS/REST with AngularJS/Hello

--- AngularJS/REST with AngularJS/Hello world/hello.js
+++ AngularJS/REST with AngularJS/Hello world/hello.js
@@ -1,8 +1,32 @@
+/* 
+ * Copyright 2016 Shlomi Reuveni.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+var url = "";
 var app = angular.module("myApp", []);
 
 app.controller("Hello", ["$scope", "$http", function($scope, $http) {
-	$http.get("http://rest-service.guides.spring.io/greeting")
-		.then(function(response) {
-			$scope.greeting = response.data;
-	});
+	$scope.helloWithName = function() {
+		if($scope.name) {
+			url = "http://rest-service.guides.spring.io/greeting?name=" + $scope.name;
+		} else {
+			url = "http://rest-service.guides.spring.io/greeting";
+		}
+		console.log("The name is: " + $scope.name + " and the url is: " + url);
+		$http.get(url)
+			.then(function(response) {
+				$scope.greeting = response.data;
+		});
+	};
 }]);
